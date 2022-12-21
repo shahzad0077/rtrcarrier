72669bfe9528f9fef48d54bf616bf6fa40927937
Ahsin Javaid <52376720+ahsinjavaid890@users.noreply.github.com>
2022-12-21 13:36:45 +0500

    Update dashboard.blade.php

--- resources/views/carrier/dashboard.blade.php
+++ resources/views/carrier/dashboard.blade.php
@@ -157,7 +157,7 @@
                 <div class="col-md-4"> 
                     <div class="row mb-3">
                         <div class="col-md-6">
-                            <a href="{{ url('job/add') }}" class="btn btn-primary mr-2 post-inner-btn form-control">
+                            <a href="{{ url('job/add') }}" class="btn btn-white btn-block text-primary">
                                 Post New Job
                             </a>
                         </div>
